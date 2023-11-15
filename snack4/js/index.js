ead599a37de9b3343243236a2e053e64fbce8214
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-11-15 12:04:53 +0100

    snack4: Esercizio completato

---
+++ snack4/js/index.js
@@ -0,0 +1,13 @@
+const array = [];
+
+for (let i = 0; i < 100; i++) {
+    if (i % 7 == 0) {
+        array.push(i);
+    } else if (i % 8 == 0) {
+        array.push(i);
+    }
+}
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
